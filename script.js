59855d135d45f734d81b290afbdb71e52e6b42d1
koecher19 <ida.koecher@gmail.com>
2022-03-05 13:48:54 +0100

    key input

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+/*
 function loadGround() {
   var groundSingle = '##############################################################################';
   var groundDouble = '##############################################################################\n##############################################################################';
   var groundTriple = '##############################################################################\n##############################################################################\n##############################################################################';
   document.getElementById('ground').innerHTML = groundTriple;
 }
+*/
 
-//window.onload = loadGround;
+function changePlayerPos() {
+  var playerPos = document.getElementsByClassName('player_box')[0];
+  /*
+    343px <-- max pos. top
+    610px <-- max üos left
+  */
+  playerPos.style.top = '343px';
+  playerPos.style.left = '610px';
+}
+
+console.log('test');
+
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode == 37) {
+    alert('Left was pressed');
+    changePlayerPos();
+  }else if (event.keyCode == 39) {
+    alert('Right was pressed');
+  }
+});
